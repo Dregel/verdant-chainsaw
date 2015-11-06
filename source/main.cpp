161cc9e5e738ef80c4347cfe9906778f5db9d209
David Bengtsson <DB.Dregel@gmail.com>
2015-11-06 08:29:38 +0100

    Small fix in distanceCalculator.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -16,6 +16,16 @@ int main()
     std::cout << distanceCalculator(2, a, b) << std::endl;
     std::cout << distanceCalculator(3, a, b) << std::endl;
     std::cout << distanceCalculator(4, a, b) << std::endl;
+
+    a.X = 10;
+    a.Y = 5;
+
+    std::cout << distanceCalculator(0, a, b) << std::endl;
+    std::cout << distanceCalculator(1, a, b) << std::endl;
+    std::cout << distanceCalculator(2, a, b) << std::endl;
+    std::cout << distanceCalculator(3, a, b) << std::endl;
+    std::cout << distanceCalculator(4, a, b) << std::endl;
+
 	return 0;
 }
 
